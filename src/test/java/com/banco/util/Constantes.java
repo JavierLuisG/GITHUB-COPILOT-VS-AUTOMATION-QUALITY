5227a5c79eca17c911f580b8608aa0d20b4d1089
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2026-03-10 11:51:49 -0500

    creacion de constantes y hooks (Add AbrirNavegador task and Constantes class)

---
+++ src/test/java/com/banco/util/Constantes.java
@@ -0,0 +1,7 @@
+package com.banco.util;
+
+public class Constantes {
+    public static final String URL = "https://jacobogarces.github.io/gen-ai-spring-front/#/";
+    public static final String ACTOR = "Luis";
+    public static final String MENSAJE_CONFIRMACION_CREACION_CUENTA = "Cuenta creada exitosamente";
+}
